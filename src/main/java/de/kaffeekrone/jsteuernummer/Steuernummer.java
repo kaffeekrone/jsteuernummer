9c552e6d4827e7fdce9b3993dbcccbb31b12b810
Phillip Wirth <pw@wunschloesung.com>
2018-12-10 10:13:12 +0100

    fixed java doc warnings

--- src/main/java/de/kaffeekrone/jsteuernummer/Steuernummer.java
+++ src/main/java/de/kaffeekrone/jsteuernummer/Steuernummer.java
@@ -5,6 +5,11 @@ public class Steuernummer {
     private final String steuernummer;
     private final Region region;
 
+    /**
+     *
+     * @param steuernummer a german steuernummer
+     * @throws SteuernummerException if the steuernummer is invalid
+     */
     private Steuernummer(String steuernummer) throws SteuernummerException {
         this.steuernummer = steuernummer;
         Region tempRegion = null;
@@ -20,6 +25,12 @@ public class Steuernummer {
         this.region = tempRegion;
     }
 
+    /**
+     *
+     * @param steuernummer a german steuernummer
+     * @param region the expected region
+     * @throws SteuernummerException if the steuernummer is invalid or does not match against the region
+     */
     private Steuernummer(String steuernummer, Region region) throws SteuernummerException {
         this.steuernummer = steuernummer;
         if (region.matchesRegion(steuernummer) || region.matchesCountry(steuernummer)) {
@@ -43,15 +54,19 @@ public class Steuernummer {
      * Guesses the region, there are some pattern overlappings for examle TH and BY do both match for 151/815/08156
      * If you care for the region, be aware of such inconveniences.
      *
+     * @param steuernummer a german steuernummer
      * @throws SteuernummerException if the supplied steuernummer is invalid
+     * @return a Steuernummer instance
      */
     public static Steuernummer of(String steuernummer) throws SteuernummerException {
         return new Steuernummer(steuernummer);
     }
 
     /**
-     *
+     * @param steuernummer a german steuernummer
+     * @param region the expected region
      * @throws SteuernummerException if the supplied steuernummer is invalid for region
+     * @return a Steuernummer instance
      */
     public static Steuernummer of(String steuernummer, Region region) throws SteuernummerException {
         return new Steuernummer(steuernummer, region);
